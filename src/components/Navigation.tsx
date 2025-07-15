b31b909bd9eec22bfe088a0a62b1111969ff172b
ayoubmp44 <contact.backvue@gmail.com>
2025-07-15 04:05:04 +0100

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,14 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 px-6">
-      
+{/*       <div className="glass-nav px-8 py-4">
+        <div className="flex items-center">
+          {/* Logo */}
+          <NavLink to="/" className="text-2xl font-bold gradient-text">
+            Backvue
+          </NavLink>
+        </div>
+      </div> */}
     </nav>
   );
 };
