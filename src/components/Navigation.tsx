d76c0ae809007d08ce0b08064d0c64ecd701fdcf
ayoubmp44 <contact.backvue@gmail.com>
2025-07-15 04:03:03 +0100

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,12 @@
 
 import { NavLink } from "react-router-dom";
 
+const Navigation = () => {
+  return (
+    <nav className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 px-6">
+      
+    </nav>
+  );
+};
 
 export default Navigation;
